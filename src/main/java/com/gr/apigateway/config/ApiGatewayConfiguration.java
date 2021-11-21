6749eb8fc1748316303ec6ce431804902a6b3dbb
Giri Babu <gbabu@flatironssolutions.com>
2021-11-21 15:19:52 +0530

    Intial commit

---
+++ src/main/java/com/gr/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,30 @@
+package com.gr.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route( r -> r.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(r -> r.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(r -> r.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(r -> r.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .route(r -> r.path("/currency-conversion-new/**")
+                        .filters( f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
